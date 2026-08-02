5ea891f777f156f60d93c569db5134ad411fce8b
Rohit Patel <rohitpatel73832@gmail.com>
2026-08-02 16:01:01 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (80.28%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -13,21 +13,39 @@
  *     }
  * }
  */
-class Solution {
+// class Solution {
 
-    TreeNode prev = null;
+//     TreeNode prev = null;
+
+//     public boolean isValidBST(TreeNode root) {
+//         if (root == null)
+//             return true;
+//         // Traverse left subtree
+//         if (!isValidBST(root.left))
+//             return false;
+//         // Current node must be greater than previous node
+//         if (prev != null && root.val <= prev.val)
+//             return false;
+//         prev = root;
+//        // Traverse right subtree
+//         return isValidBST(root.right);
+//     }
+// }
+class Solution {
+    public boolean helper(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+               
+    }
 
     public boolean isValidBST(TreeNode root) {
-        if (root == null)
-            return true;
-        // Traverse left subtree
-        if (!isValidBST(root.left))
-            return false;
-        // Current node must be greater than previous node
-        if (prev != null && root.val <= prev.val)
-            return false;
-        prev = root;
-       // Traverse right subtree
-        return isValidBST(root.right);
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 }

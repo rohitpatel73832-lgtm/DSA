f9486528d436c919e0807059bd44f6382bcb1cf4
Rohit Patel <rohitpatel73832@gmail.com>
2026-08-11 01:45:29 +0530

    Time: 7 ms (97.90%) | Memory: 47.2 MB (47.89%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -8,28 +8,31 @@ class Solution {
         int down=0;
         int up=0;
         if (sc + 1 < ec && matrix[sr][sc + 1] > matrix[sr][sc]) {
-            right = helper(sr, sc + 1, er, ec, matrix,dp);
+            right =1+ helper(sr, sc + 1, er, ec, matrix,dp);
         }
 
         // move left
         if (sc - 1 >= 0 && matrix[sr][sc - 1] > matrix[sr][sc]) {
-            left = helper(sr, sc - 1, er, ec, matrix,dp);
+            left =1+ helper(sr, sc - 1, er, ec, matrix,dp);
         }
 
         // move down
         if (sr + 1 < er && matrix[sr + 1][sc] > matrix[sr][sc]) {
-            down = helper(sr + 1, sc, er, ec, matrix,dp);
+            down = 1+ helper(sr + 1, sc, er, ec, matrix,dp);
         }
 
         // move up
         if (sr - 1 >= 0 && matrix[sr - 1][sc] > matrix[sr][sc]) {
-            up = helper(sr - 1, sc, er, ec, matrix,dp);
+            up = 1+ helper(sr - 1, sc, er, ec, matrix,dp);
         }
 
         int a = Math.max(right, left);
         int b = Math.max(up, down);
+        if (a == 0 && b == 0) {
+            return dp[sr][sc] = 1;
+        }
 
-        return dp[sr][sc]= 1 + Math.max(a, b);
+        return dp[sr][sc]=  Math.max(a, b);
     }
     public int longestIncreasingPath(int[][] matrix) {
         int m=matrix.length;

56abbadc70cb463fac4a3e02936feeab0d3d2bd2
Rohit Patel <rohitpatel73832@gmail.com>
2026-08-24 00:20:02 +0530

    Time: 1 ms (73.19%) | Memory: 43 MB (25.01%) - LeetSync

---
+++ 504-base-7/base-7.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String convertToBase7(int num) {
+
+        if (num == 0) {
+            return "0";
+        }
+
+        boolean negative = num < 0;
+
+        if (negative) {
+            num = -num;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (num > 0) {
+            int rem = num % 7;
+            sb.append(rem);
+            num = num / 7;
+        }
+
+        if (negative) {
+            sb.append("-");
+        }
+
+        return sb.reverse().toString();
+    }
+}

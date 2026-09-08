4849ff9438c3168325741b7b6a33314df9db39f4
Rohit Patel <rohitpatel73832@gmail.com>
2026-09-08 18:46:10 +0530

    Time: 1 ms (100.00%) | Memory: 47.1 MB (94.09%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.java
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.java
@@ -2,26 +2,60 @@ class Solution {
     public int[] resultsArray(int[] nums, int k) {
       int n=nums.length;
       int[] ans= new int[n-k+1];
-      for(int i=0; i<=n-k; i++){
-        int curr = nums[i];
-            boolean valid = true;
+      Arrays.fill(ans,-1);
 
-            for (int j = i + 1; j < i + k; j++) {
+      //loop method
+    //   for(int i=0; i<=n-k; i++){
+    //     int curr = nums[i];
+    //         boolean valid = true;
 
-                if (nums[j] == curr + 1) {
-                    curr = nums[j];
-                } else {
-                    valid = false;
-                    break;
-                }
-            }
+    //         for (int j = i + 1; j < i + k; j++) {
 
-            if (valid) {
-                ans[i] = curr;
-            } else {
-                ans[i] = -1;
-            }
-      }
-      return ans;
+    //             if (nums[j] == curr + 1) {
+    //                 curr = nums[j];
+    //             } else {
+    //                 valid = false;
+    //                 break;
+    //             }
+    //         }
+
+    //         if (valid) {
+    //             ans[i] = curr;
+    //         } else {
+    //             ans[i] = -1;
+    //         }
+    //   }
+    //   return ans;
+
+    // by sliding window
+
+    int count=1;
+    for(int i=1;i<k; i++){
+        if(nums[i]==nums[i-1]+1){
+            count++;
+        }else{
+            count=1;
+        }
+    }
+    if(count==k){
+        ans[0]=nums[k-1];
+    }
+
+    int l=1; 
+    int r=k;
+    while(r<n){
+        if(nums[r-1]+1==nums[r]){
+            count++;
+        }else{
+            count=1;
+        }
+        if(count>=k){
+            ans[l]=nums[r];
+        }
+        l++;
+        r++;
+    }
+    return ans;
+
     }
 }

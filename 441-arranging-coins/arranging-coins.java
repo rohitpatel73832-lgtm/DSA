8a2e53bdfe3248deaf4d8a9231f81405a96e7307
Rohit Patel <rohitpatel73832@gmail.com>
2026-09-10 02:02:37 +0530

    Time: 11 ms (7.68%) | Memory: 42.4 MB (90.87%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,9 +1,19 @@
 class Solution {
     public int arrangeCoins(int n) {
-        double k=(-1 +Math.sqrt(1+8.0*n))/2;
-        double ans=Math.floor(k);
-        return (int)ans;
-        
+        // double k=(-1 +Math.sqrt(1+8.0*n))/2;
+        // double ans=Math.floor(k);
+        // return (int)ans;
+        int sum=0;
+        for(int i=1; i<=n; i++){
+            sum+=i*1;
+            if(n-sum==0){
+                return i;
+            }
+            if(n-sum<=i){
+                return i;
+            }
+        }
+        return -1;
         
     }
 }
